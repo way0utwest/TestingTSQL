1541ddc70cdf12b496d517de48ad898099147073
way0utwest <sjones@sqlservercentral.com>
2014-08-19 14:11:37 -0600

    Header and rearrange script
    Mostly formatting changes. rearranged a few things, cleaned up the
    visual display of code

--- SQT_RefactorArticles_MetaData_Demo.sql
+++ SQT_RefactorArticles_MetaData_Demo.sql
@@ -1,5 +1,18 @@
--- refactor the article reading time.
--- we don't need to calculate this every time.
+/*
+SQT - build test classes
+
+Copyright 2014, Steve Jones and Red Gate Software
+
+This code is provided as is for demonstration purposes. It may not be suitable for
+your environment. Please test this on your own systems. This code may not be republished 
+or redistributed by anyone without permission.
+You are free to use this code inside of your own organization.
+
+
+Description:
+This script will refactor the database to help with the article reading time. we don't need to calculate this every time.
+This will also add a test class and test for the Simple Talk demo application.
+*/
 
 -- check Articles table
 select
@@ -11,13 +24,20 @@ select
     O.object_id = C.object_id
   where
     o.name = 'Articles';
+
 -- we need to add [ReadingTime] to Articles
 
 
 
 
+
+
+
+
+
+
 -- Let's write a test
--- new class
+-- First, create a new class
 -- use SQL Test to create new test and class or run this code:
 -- Class: Articles
 exec tsqlt.NewTestClass @ClassName = N'Articles';
@@ -35,7 +55,7 @@ exec tsqlt.NewTestClass @ClassName = N'Articles';
 --          Start Test  
 --------------------------------------------------------------------------
 --------------------------------------------------------------------------
- -- create test
+ -- create test using stub or SQL Test
  -- class: Articles
  -- Name: Articles_Check_metadata
 
@@ -50,7 +70,7 @@ exec tsqlt.NewTestClass @ClassName = N'Articles';
 	[PublishDate] [DATETIME] NULL,
 	[ModifiedDate] [DATETIME] NULL,
 	[URL] [CHAR](200) NULL,
-	[Comments] [INT] NULL,
+	[Comments] [INT] NULL
   );
   
   --Act
@@ -68,53 +88,14 @@ EXEC tsqlt.AssertResultSetsHaveSameMetaData @expectedCommand = N'select * from A
 
 -- Start refactoring
 -- alter table to store times.
-ALTER TABLE dbo.Articles
- ADD ReadingTimeEstimate TIME;
+/*
+alter TABLE dbo.Articles
+ADD ReadingTimeEstimate TIME;
+*/
 
 
-- retest
-exec tsqlt.RunAll;
 
-
--- we need to fix our test.
---------------------------------------------------------------------------
---------------------------------------------------------------------------
---          Start Test  
---------------------------------------------------------------------------
---------------------------------------------------------------------------
- -- create test
- -- class: Articles
- -- Name: check table meta data
-
-  --Assemble
- CREATE TABLE Articles.Expected
-  (
-	[ArticlesID] [INT] IDENTITY(1,1) NOT NULL,
-	[AuthorID] [INT] NULL,
-	[Title] [CHAR](142) NULL,
-	[Description] [VARCHAR](MAX) NULL,
-	[Article] [VARCHAR](MAX) NULL,
-	[PublishDate] [DATETIME] NULL,
-	[ModifiedDate] [DATETIME] NULL,
-	[URL] [CHAR](200) NULL,
-	[Comments] [INT] NULL,
-	[ReadingTimeEstimate] [TIME](7) NULL,
-
-  );
-  
-  --Act
-  
-  --Assert
-EXEC tsqlt.AssertResultSetsHaveSameMetaData
-  @expectedCommand = N'select * from Articles.Expected', -- nvarchar(max)
-  @actualCommand = N'select * from articles' -- nvarchar(max)
-  
-
---------------------------------------------------------------------------
---------------------------------------------------------------------------
---          End Test  
---------------------------------------------------------------------------
---------------------------------------------------------------------------
+-- we need to fix our test first
 
 --------------------------------------------------------------------------
 --------------------------------------------------------------------------
@@ -137,14 +118,14 @@ EXEC tsqlt.AssertResultSetsHaveSameMetaData
 	[ModifiedDate] [DATETIME] NULL,
 	[URL] [CHAR](200) NULL,
 	[Comments] [INT] NULL,
-	[ReadingTimeEstimate] [TIME](7) NULL,
-
+	[ReadingTimeEstimate] [TIME](7) NULL
   );
   
   --Act
   
   --Assert
-EXEC tsqlt.AssertResultSetsHaveSameMetaData @expectedCommand = N'select * from Articles.Expected', -- nvarchar(max)
+EXEC tsqlt.AssertResultSetsHaveSameMetaData
+  @expectedCommand = N'select * from Articles.Expected', -- nvarchar(max)
   @actualCommand = N'select * from articles' -- nvarchar(max)
   
 
@@ -154,6 +135,15 @@ EXEC tsqlt.AssertResultSetsHaveSameMetaData @expectedCommand = N'select * from A
 --------------------------------------------------------------------------
 --------------------------------------------------------------------------
 
+-- run test
+exec tsqlt.Run @TestName = N'Articles.[test Articles_Check_metadata]';
+
+-- refactor table
+alter table dbo.Articles
+add ReadingTimeEstimate time;
+
+-- retest
+exec tsqlt.RunAll;
 
 
 --------------------------------------------------------------------------
